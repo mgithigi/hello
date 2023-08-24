2fa67a46aa6accab3967ac02bc07c330ed7eb925
Your Name <youremail@huskers.unl.edu>
2023-08-24 18:20:12 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Melvyn Githgi
+ *         mgithigi2husker@unl.edu
+ * Date: 2023/24/08
+ *
+ * A simple Name/Major program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello I am Melvyn Githigi\n");
+  printf("My Major is Electrical Engineering\n");
+
+  return 0;
+}
